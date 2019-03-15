85095a04bc755908abdcfb6b82ad15c6d51a0813
Daniel Vasic <dvasic1@gmail.com>
2019-03-15 13:31:11 +0100

    Update LoginController.java

--- src/postRecord/view/LoginController.java
+++ src/postRecord/view/LoginController.java
@@ -75,7 +75,7 @@ public class LoginController implements Initializable {
                     titleLabel.setText("Greška: Korisničko ime i lozinka nisu ispravni.");
                     titleLabel.setStyle("-fx-background-color:#d32f2f; -fx-text-fill:white");
                 } else {
-                    LoginController.userid = rs.getInt("1");
+                    LoginController.userid = rs.getInt(1);
                     if("1".equals(role)){
                         closeStage();
                         loadAdmin();
